34e370ab39a3d9bee850c2bdacccc3a9e40c8fad
Nirmal <fernandonirmal345@gmail.com>
2024-11-26 00:54:43 +0530

    Convert BGR to HSV

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ cap = cv2.VideoCapture(2)
 while True:
     ret, frame = cap.read()
     
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
     cv2.imshow('Webcam Feed', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break

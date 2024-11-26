ba79d2ffe5940326bd7739fd00b412065bc20e19
Nirmal <fernandonirmal345@gmail.com>
2024-11-26 10:24:46 +0530

    Add red colour we going to detect

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import cv2
 from util import get_limits
 
+red = [0,0,255]
+
 cap = cv2.VideoCapture(2)
 
 while True:
     ret, frame = cap.read()
     
     hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lowerLimit, upperLimit = get_limits(color=red)
 
     cv2.imshow('Webcam Feed', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):

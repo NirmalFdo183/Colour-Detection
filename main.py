d01283fe97945437db1ec09cc94be075fcfd2271
Nirmal <fernandonirmal345@gmail.com>
2024-11-26 10:38:32 +0530

    Import from Pillow Library

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import cv2
+from PIL import Image
 from util import get_limits
 
 red = [0,0,255]
 
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
     
     hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
-
     lowerLimit, upperLimit = get_limits(color=red)
+
+    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
 
     cv2.imshow('Webcam Feed', mask)
     if cv2.waitKey(1) & 0xFF == ord('q'):

1fb8f6d9cafb9acbccc2f45c81571985beb83d09
Nirmal <fernandonirmal345@gmail.com>
2024-11-26 11:08:24 +0530

    Change detect colour to yellow

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 from PIL import Image
 from util import get_limits
 
-red = [0,0,255]
+yellow = [255,255,0]
 
 cap = cv2.VideoCapture(0)
 
@@ -11,7 +11,7 @@ while True:
     
     hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lowerLimit, upperLimit = get_limits(color=red)
+    lowerLimit, upperLimit = get_limits(color=yellow)
 
     mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
 
@@ -26,7 +26,7 @@ while True:
 
         frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
 
-    cv2.imshow('Webcam Feed', mask)
+    cv2.imshow('Webcam Feed', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
